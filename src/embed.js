1958a18b155267448e6390a286a9e15055f22250
Yehuda Neufeld <yehuda@yiddishe-kop.com>
2023-11-05 17:24:13 +0200

    fix: get script origin

--- src/embed.js
+++ src/embed.js
@@ -2,8 +2,19 @@ import { createApp } from "vue";
 import Captcha from "./components/Captcha.vue";
 import "./assets/main.css";
 
+function getScriptUrl() {
+  if (document.currentScript) {
+    const scriptSrc = document.currentScript.src;
+    return new URL(scriptSrc);
+  } else {
+    // If document.currentScript is not available (e.g., in some older browsers), you might need a fallback.
+    return window.location.href;
+  }
+}
+
 function loadStyles() {
-  const domain = window.location.origin;
+  const url = getScriptUrl();
+  const domain = url.origin;
   const style = document.createElement("link");
   style.rel = "stylesheet";
   style.href = `${domain}/assets/main.css`;
@@ -20,7 +31,7 @@ function mountIsraelCaptcha() {
 // Mount the captcha manually using the global function
 window.mountIsraelCaptcha = mountIsraelCaptcha;
 
-const urlParams = new URLSearchParams(window.location.search);
+const urlParams = new URLSearchParams(getScriptUrl().search);
 const shouldAutoMount = urlParams.get("mount");
 
 if (shouldAutoMount) {

da3622671b9a8ae51b470f819ef1b188431bc3f1
Yehuda Neufeld <yehuda@yiddishe-kop.com>
2023-11-05 17:36:25 +0200

    fix

--- src/embed.js
+++ src/embed.js
@@ -24,7 +24,7 @@ function mountIsraelCaptcha() {
 // Mount the captcha manually using the global function
 window.mountIsraelCaptcha = mountIsraelCaptcha;
 
-const urlParams = new URLSearchParams(getScriptUrl().searchParams);
+const urlParams = getScriptUrl().searchParams;
 const shouldAutoMount = urlParams.get("mount");
 
 if (shouldAutoMount) {

e829046c7d72c5e69b406b9829268a8f3d616bf0
Yehuda Neufeld <yehuda@yiddishe-kop.com>
2023-11-05 17:33:52 +0200

    fix: import.meta.url

--- src/embed.js
+++ src/embed.js
@@ -3,23 +3,14 @@ import Captcha from "./components/Captcha.vue";
 import "./assets/main.css";
 
 function getScriptUrl() {
-  console.log("getScriptUrl", document.currentScript, import.meta.url);
-  if (document.currentScript) {
-    const scriptSrc = document.currentScript.src;
-    const url = new URL(scriptSrc);
-    console.log({ scriptSrc, url, origin: url.origin });
-  } else {
-    // If document.currentScript is not available (e.g., in some older browsers), you might need a fallback.
-    return window.location.href;
-  }
+  return new URL(import.meta.url);
 }
-const DOMAIN = "https://support-israel-captcha.vercel.app";
 
 function loadStyles() {
   const url = getScriptUrl();
   const style = document.createElement("link");
   style.rel = "stylesheet";
-  style.href = `${DOMAIN}/assets/main.css`;
+  style.href = `${url.origin}/assets/main.css`;
   document.head.appendChild(style);
 }
 

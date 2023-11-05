1ef16b92e0751d5985cc7fee623118f32d56156a
Yehuda Neufeld <yehuda@yiddishe-kop.com>
2023-11-05 12:18:32 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="">
+    <input type="checkbox" v-model="checkbox" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const checkbox = ref(false);
+</script>
